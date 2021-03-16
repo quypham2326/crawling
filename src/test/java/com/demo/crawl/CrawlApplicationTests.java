f9b8c97d7c1f5716e90d0ae8066d4709d4dfef60
DESKTOP-RCFES7K\Admin <quypham2326@gmail.com>
2021-03-16 20:05:32 +0700

    Create prọect

---
+++ src/test/java/com/demo/crawl/CrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.crawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
